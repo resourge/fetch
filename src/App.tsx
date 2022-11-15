1527d469211512897b1918c2564c2213347c8e90
resourge <jdaoliveiraprograming@hotmail.com>
2022-11-15 16:20:17 +0000

    refactor(app): clean app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import { HttpService } from 'packages/http-service/src';
-HttpService.defaultConfig.threshold = 0;
-HttpService.upload('POST', 'https://dummyjson.com/products/1', [], {
-	test: 12 
-}).then(() => {
-	console.log('then1')
-})
 function App() {
 	return (
 		<div className="App">
