5eb6b3888ab5af119f6a8cd0cb5df23ff09f7eb7
resourge <jdaoliveiraprograming@hotmail.com>
2023-09-27 11:15:44 +0100

    feat(globalloader): change to portal instead of a simple component

--- packages/react-fetch/src/components/globalLoader/GlobalLoader.tsx
+++ packages/react-fetch/src/components/globalLoader/GlobalLoader.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { createPortal } from 'react-dom';
 
 import Loader from '../Loader';
 
@@ -16,29 +18,45 @@ export type GlobalLoaderProps = BaseGlobalLoaderProps<React.CSSProperties>
 const GlobalLoader: React.FC<GlobalLoaderProps> = ({
 	loaderId, style, children, color = globalColor
 }) => {
+	const [portalContainer] = useState(() => {
+		let portalContainer = document.getElementById('global-loader');
+
+		if ( !portalContainer ) {
+			portalContainer = document.createElement('div');
+
+			portalContainer.id = 'global-loader';
+
+			document.body.appendChild(portalContainer)
+		}
+
+		return portalContainer
+	})
 	const globalLoading = setGlobalLoading(children ?? <GlobalLoading color={color} />)
 
 	return (
 		<Loader 
 			loaderId={loaderId}
 			loadingElement={(
-				<div 
-					style={{
-						position: 'fixed',
-						top: 0,
-						left: 0,
-						right: 0,
-						bottom: 0,
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-						backgroundColor: 'rgba(0,0,0,0.5)',
-						zIndex: 10000,
-						...style
-					}}
-				>
-					{ globalLoading }
-				</div>
+				createPortal(
+					<div 
+						style={{
+							position: 'fixed',
+							top: 0,
+							left: 0,
+							right: 0,
+							bottom: 0,
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							backgroundColor: 'rgba(0,0,0,0.5)',
+							zIndex: 10000,
+							...style
+						}}
+					>
+						{ globalLoading }
+					</div>,
+					portalContainer
+				)
 			)}
 		/>
 	)
