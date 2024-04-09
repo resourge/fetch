aa2b3eb46f2a12343376354d71051c070950f043
resourge <jdaoliveiraprograming@hotmail.com>
2024-04-09 16:02:39 +0100

    fix(usefetch): fix on hotreload

--- packages/react-fetch/src/hooks/useRefMemo.ts
+++ packages/react-fetch/src/hooks/useRefMemo.ts
@@ -1,10 +1,17 @@
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 export const useRefMemo = <T>(cb: () => T): T => {
 	const memoRef = useRef<T | undefined>(undefined);
 	if ( !memoRef.current ) {
 		memoRef.current = cb();
 	}
+
+	// To fix hot-reload not working as expected
+	useEffect(() => {
+		return () => {
+			memoRef.current = undefined;
+		}
+	}, [])
 
 	return memoRef.current;
 }
