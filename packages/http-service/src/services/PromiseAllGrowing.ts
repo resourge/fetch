eb0669db296c1dd7be26145ba2d015eeb1a0097e
resourge <jdaoliveiraprograming@hotmail.com>
2026-01-13 10:11:08 +0000

    fix(promiseallgrowing): fix Promises not being finished at the same order they started

--- packages/http-service/src/services/PromiseAllGrowing.ts
+++ packages/http-service/src/services/PromiseAllGrowing.ts
@@ -1,6 +1,7 @@
 type PromisePoolType = 'useEffect' | 'request' | 'fetch'
 
 type PromisePoolValueType = {
+	p: Promise<any>
 	pending: Set<Promise<any>>
 	waiters: Array<(...args: any[]) => any>
 }
@@ -9,48 +10,60 @@ export class PromiseAllGrowing {
 	private readonly pools: Record<PromisePoolType, PromisePoolValueType> = {
 		useEffect: {
 			pending: new Set<Promise<any>>(),
-			waiters: []
+			waiters: [],
+			p: Promise.resolve()
 		},
 		request: {
 			pending: new Set<Promise<any>>(),
-			waiters: []
+			waiters: [],
+			p: Promise.resolve()
 		},
 		fetch: {
 			pending: new Set<Promise<any>>(),
-			waiters: []
+			waiters: [],
+			p: Promise.resolve()
 		}
 	} 
 
-	async promise<T>(pool: PromisePoolType, p: () => Promise<T>): Promise<T> {
+	async promise<T>(key: PromisePoolType, p: () => Promise<T>): Promise<T> {
 		const tracked = p()
 
-		this.pools[pool].pending.add(tracked);
+		const pool = this.pools[key];
+		pool.pending.add(tracked);
 
-		const [re] = await Promise.all([
+		pool.p = Promise.all([
 			tracked
 			.finally(() => {
-				this.pools[pool].pending.delete(tracked);
+				pool.pending.delete(tracked);
 				this.check(pool);
 			}),
 			this.waitAll(pool)
-		]);
+		])
+
+		const [re] = await pool.p;
 
 		return re;
 	}
 
-	public waitAll(pool: PromisePoolType) {
-		return new Promise((resolve) => {
-			this.pools[pool].waiters.push(resolve);
+	public waitAll(pool: PromisePoolValueType) {
+		const oldP = pool.p;
+		return new Promise<void>((resolve) => {
+			pool.waiters.push(async () => {
+				await oldP;
+				resolve();
+			});
 			this.check(pool);
 		});
 	}
 
-	private check(pool: PromisePoolType) {
-		if (this.pools[pool].pending.size === 0 && this.pools[pool].waiters.length > 0) {
-			this.pools[pool].waiters.forEach(resolve => {
-				resolve(); 
+	private check(pool: PromisePoolValueType) {
+		if (pool.pending.size === 0 && pool.waiters.length > 0) {
+			// eslint-disable-next-line @typescript-eslint/no-misused-promises
+			pool.waiters.forEach(async (resolve) => {
+				await resolve(); 
 			});
-			this.pools[pool].waiters = [];
+			pool.p = Promise.resolve()
+			pool.waiters = [];
 		}
 	}
 };
